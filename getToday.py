6f80aff4a58690aee1974073924211184b75a676
Mattias Hållsten <hallsten.mattias@gmail.com>
2019-12-23 17:32:00 +0100

    Changed variable names, better boolean

--- getToday.py
+++ getToday.py
@@ -12,16 +12,16 @@ def makeImagesDir():
 
 def downloadImage():
     date = datetime.date.today()
-    url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/2019/12/malaysia-borneo-palm-oil-terrace/'
+    url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
     
-    for link in soup.findAll('meta'):
-        content = link.get('content')
-        if content.endswith('1900.1.jpg'):
+    for meta in soup.findAll('meta'):
+        link = meta.get('content')
+        if meta.get('property') == "og:image":
             print('Downloading...')
             f = open('images/' + str(date) + '.jpg' ,'wb')
-            f.write(urllib.request.urlopen(content).read())
+            f.write(urllib.request.urlopen(link).read())
             f.close()
             print('Downloaded, put in images directory!')
 
