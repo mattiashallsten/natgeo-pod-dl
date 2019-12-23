aef7eae04873c0623f206500154d66969b28a69c
Mattias Hållsten <hallsten.mattias@gmail.com>
2019-12-23 17:50:37 +0100

    Added comments

--- getToday.py
+++ getToday.py
@@ -12,14 +12,20 @@ def makeImagesDir():
 
 def downloadImage():
     date = datetime.date.today()
+
     url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
+        
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
-    
+
+    # Read through the object created by soup, find every meta tag
     for meta in soup.findAll('meta'):
         link = meta.get('content')
+        # If the meta tag has the property "og:image", download the file
         if meta.get('property') == "og:image":
             print('Downloading...')
+
+            # Open a file in the images directory, define a name, download the file
             f = open('images/' + str(date) + '.jpg' ,'wb')
             f.write(urllib.request.urlopen(link).read())
             f.close()
